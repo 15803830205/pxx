2cb2083a6f587faac57db99d211757ed4cbfbcdf
shop <https://github.com/15803830205/pxx.git>
2020-03-13 22:06:35 +0800

    订单

---
+++ takeout-order/takeout-server/src/main/java/com.pan/repository/OrderDetailRepository.java
@@ -0,0 +1,13 @@
+package com.pan.repository;
+
+
+
+import com.pan.dataobject.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, String> {
+
+	List<OrderDetail> findByOrderId(String orderId);
+}
